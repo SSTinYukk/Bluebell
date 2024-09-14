705fd9db35c2dbbe219c795be53eab7c9aa79505
SSTinYukk <13999730523@163.com>
2024-09-14 16:53:36 +0800

    refactor:  使用sony雪花算法

---
+++ pkg/snowflake.go/gen_id.go
@@ -0,0 +1,39 @@
+package snowflakego
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sony/sonyflake"
+)
+
+var (
+	sonyFlake     *sonyflake.Sonyflake
+	sonyMachineID uint16
+)
+
+func getMachineId() (uint16, error) {
+	return sonyMachineID, nil
+}
+func Init(startTime string, machineId uint16) (err error) {
+	sonyMachineID = machineId
+	var st time.Time
+	st, err = time.Parse("2006-01-02", startTime)
+	if err != nil {
+		return err
+	}
+	settings := sonyflake.Settings{
+		StartTime: st,
+		MachineID: getMachineId,
+	}
+	sonyFlake = sonyflake.NewSonyflake(settings)
+	return
+}
+func GenID() (id uint64, err error) {
+	if sonyFlake == nil {
+		err = fmt.Errorf("sony flake not inited")
+		return
+	}
+	id, err = sonyFlake.NextID()
+	return
+}
