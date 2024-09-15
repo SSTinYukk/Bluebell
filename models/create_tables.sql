607711d0d87d3d9bf374f1bb01192eeff79f24cb
SSTinYukk <13999730523@163.com>
2024-09-15 17:40:54 +0800

    feat:add for create tables

---
+++ models/create_tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXIST `user`
+CREATE TABLE `user`(
+    `id` bigint(20) NOT NULL,
+    `user_id` bigint(20) NOT NULL,
+    `username` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+    `email` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+    `gender` tinyint(4) NOT NULL DEFAULT '0',
+    `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP UPDATE ON CURRENT_TIMESTAMP,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY  `idx_username` (`username`) USING IN BTREE,
+    UNIQUE KEY  `idx_user_id` (`user_id`) USING BTREE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
