8992aadca44d455472dfbfbda4328fd41a008e52
SSTinYukk <13999730523@163.com>
2024-09-17 17:46:51 +0800

    feat:create user tables through mysql

--- models/create_tables.sql
+++ models/create_tables.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXIST `user`
 CREATE TABLE `user`(
     `id` bigint(20) NOT NULL,
     `user_id` bigint(20) NOT NULL,
-    `username` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+    `username` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,             
     `email` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
     `gender` tinyint(4) NOT NULL DEFAULT '0',
     `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
